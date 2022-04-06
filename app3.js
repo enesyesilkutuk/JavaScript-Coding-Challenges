309c953031a5bd6082ae5de3597a743b4250b927
enesyesilkutuk <enesyesilkutuk@gmail.com>
2022-04-06 22:17:42 +0300

    solution-2 added

--- app3.js
+++ app3.js
@@ -21,3 +21,32 @@ for (let i=0; i < count; i++){
 }
 
 console.log(fibonacci);
+
+// Solution-2 :
+
+let count2 = +prompt("Enter a number");
+const fibonacci2 = [];
+
+for (let i=0; i < count2; i++){
+
+    while ((fibonacci2.length == 0) || (fibonacci2.length == 1)) {
+
+        fibonacci2.push(i);
+        break;
+    }
+
+    if (fibonacci2.length == 1) continue;
+
+    else if (fibonacci2.length == 2) {
+
+        fibonacci2[i+1] = fibonacci2[i] + fibonacci2[i-1];
+    }
+
+    else {
+
+        fibonacci2[i] = fibonacci2[i-2] + fibonacci2[i-1];
+    }
+
+}
+
+console.log(fibonacci2);
