b9ac877b80f030de7a010a3fe76099ea2108ebf3
enesyesilkutuk <enesyesilkutuk@gmail.com>
2023-01-27 23:56:28 +0300

    app10.js added

---
+++ app10.js
@@ -0,0 +1,12 @@
+// Write a function that checks if the date given as a parameter is valid or not
+
+function isDateValid(date) {
+    return !Number.isNaN(new Date(date).valueOf());
+};
+
+console.log(isDateValid("December 27, 2022 15:00:00")); // returns true
+console.log(isDateValid("December 32, 2022 15:00:00")); // returns false
+
+// Here we have a function that will take a date string and return whether that date is valid or not,
+
+// In this example, we can see that the 32nd of December doesn't exist.
