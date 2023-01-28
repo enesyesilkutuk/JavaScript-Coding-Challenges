79f49054c8c397cd87a46f1ee8198236b64bc29e
enesyesilkutuk <enesyesilkutuk@gmail.com>
2023-01-29 00:43:31 +0300

    new func added

--- app10.js
+++ app10.js
@@ -7,6 +7,17 @@ function isDateValid(date) {
 console.log(isDateValid("December 27, 2022 15:00:00")); // returns true
 console.log(isDateValid("December 32, 2022 15:00:00")); // returns false
 
-// Here we have a function that will take a date string and return whether that date is valid or not,
+// Here we have a function that will take a date string and return whether that date is valid or not.
 
 // In this example, we can see that the 32nd of December doesn't exist.
+
+// -----------------------------------------------------------------------
+
+// Javascript has no built-in capitalize function. Write a function that only converts the first letter of the string given as a parameter to upper-case.
+
+function capitalize(str) {
+    str = str.toLocaleLowerCase();
+    return str.charAt(0).toLocaleUpperCase() + str.slice(1);
+};
+
+console.log(capitalize("wasHİngTOn")); // returns Washington
